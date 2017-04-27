6647f9acc8fcaa0aaab03a8305854b823bf0b129
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2017-04-27 17:08:48 -0500

    Create LinkageResult class; add attributes to Job class

--- src/app/job.ts
+++ src/app/job.ts
@@ -1,18 +1,26 @@
+import { LinkageResult } from './linkage-result';
+
 export class Job {
   id: number;
   kind: string;
   status: string;
+  error: string;
   linkageId: number;
+  linkageResultId: number;
   startedAt: Date;
   endedAt: Date;
+  linkageResult: LinkageResult;
 
   private static attributeMap = {
     id: "id",
     kind: "kind",
     status: "status",
+    error: "error",
     linkage_id: "linkageId",
+    linkage_result_id: "linkageResultId",
     started_at: "startedAt",
-    ended_at: "endedAt"
+    ended_at: "endedAt",
+    linkage_result: "linkageResult"
   };
 
   constructor(attribs: any) {
@@ -20,6 +28,11 @@ export class Job {
       let value = attribs[key];
       if (key in Job.attributeMap) {
         let mappedKey = Job.attributeMap[key];
+        switch (mappedKey) {
+          case 'linkageResult':
+            value = new LinkageResult(value);
+            break;
+        }
         this[mappedKey] = value;
       }
     }
