6d799d11dd70546c2d49b52404a946a3d9f5f053
Jeremy Stephens <jeremy.f.stephens@vumc.org>
2018-06-27 16:16:04 -0500

    Add data preview to DatasetDetailComponent

--- src/app/dataset-detail/dataset-detail.component.ts
+++ src/app/dataset-detail/dataset-detail.component.ts
@@ -6,6 +6,7 @@ import { switchMap } from 'rxjs/operators';
 import { Dataset, DatasetKind } from '../dataset';
 import { DatasetService } from '../dataset.service';
 import { ClientError, ValidationError } from '../errors';
+import { DataColumnType, DataColumn, DataTable } from '../data-table';
 
 @Component({
   selector: 'app-dataset-detail',
@@ -16,6 +17,8 @@ export class DatasetDetailComponent implements OnInit {
   dataset: Dataset;
   clientError: ClientError;
   validationError: ValidationError;
+  dataTable: DataTable;
+  numPages: number;
 
   constructor(
     private datasetService: DatasetService,
@@ -27,15 +30,30 @@ export class DatasetDetailComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.pipe(
       switchMap((params: Params) => {
-        return this.datasetService.getDataset(+params['id'], false);
+        return this.datasetService.getDataset(+params['id'], true);
       })).
       subscribe(result => {
         if (result instanceof Dataset) {
           this.dataset = result;
+          this.setupDataTable();
         } else if (result instanceof ClientError) {
           this.clientError = result;
         }
       });
+  }
+
+  setupDataTable(): void {
+    let dataTable = new DataTable(this.dataset.fields);
+    this.datasetService.getRecords(this.dataset.id, 10, 0).subscribe(result => {
+      if (result instanceof ClientError) {
+        this.clientError = result;
+      } else if (result instanceof ValidationError) {
+        this.validationError = result;
+      } else {
+        dataTable.addRows(result);
+        this.dataTable = dataTable;
+      }
+    });
   }
 
   edit(): void {
