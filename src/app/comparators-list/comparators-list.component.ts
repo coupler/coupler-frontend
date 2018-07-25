d3f9bffcc57d62c048e86f543199882ff025edf8
Jeremy Stephens <jeremy.f.stephens@vumc.org>
2018-07-25 15:35:32 -0500

    Tweak style in ComparatorsListComponent

--- src/app/comparators-list/comparators-list.component.ts
+++ src/app/comparators-list/comparators-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Linkage } from '../linkage';
+import { Comparator } from '../comparator';
 
 @Component({
   selector: 'app-comparators-list',
@@ -11,6 +12,14 @@ export class ComparatorsListComponent {
   @Input() linkage: Linkage;
 
   constructor(private router: Router) { }
+
+  set1(comparator: Comparator): string {
+    return comparator.sets.map(s => s.field1).join(", ");
+  }
+
+  set2(comparator: Comparator): string {
+    return comparator.sets.map(s => s.field2).join(", ");
+  }
 
   gotoDetail(id: number): void {
     this.router.navigate(['/linkages', this.linkage.id, 'comparators', id]);
